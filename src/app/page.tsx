7c214584b7b18848d67d89d1d9a3eb6046fb4199
sebasmzg <j.sebastianmzg@gmail.com>
2024-09-07 11:08:39 -0500

    add navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import NavBar from "@/components/common/navbar/navBar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <main >
+       main
+      </main>
+      <footer>
+        footer 
+      </footer>
+    </div>
+  );
+}
