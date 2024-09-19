1f495533d8654ae336fe5a1b3fc9d5bb79ac4cef
mariaisva2 <mariac20160@gmail>
2024-09-18 21:21:34 -0500

    add form styles

--- src/components/eventregistration/eventregistrationForm.tsx
+++ src/components/eventregistration/eventregistrationForm.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import styled from 'styled-components';
 import { FaUser, FaEnvelope, FaPhone, FaMapMarkerAlt, FaCalendarAlt, FaListUl } from 'react-icons/fa';
@@ -7,37 +7,36 @@ const EventRegistrationForm = () => {
   return (
     <Container>
       <ImageSection>
-        <Overlay>
-          <Phrase>¡Únete a la aventura y regístrate en nuestro próximo evento!</Phrase>
-        </Overlay>
-        <img src="/woman.jpg" alt="Event" />
+        <OverlayText>
+          ¡Únete a la aventura y regístrate en nuestro próximo evento!
+        </OverlayText>
+        <img src="/img/woman.jpg" alt="Event" />
       </ImageSection>
       <FormSection>
-
         <FormContainer>
           <Form>
             <Field>
-              <IconWrapper><FaUser /></IconWrapper>
+              <Icon><FaUser /></Icon>
               <Input type="text" placeholder="Nombre del participante" />
             </Field>
 
             <Field>
-              <IconWrapper><FaUser /></IconWrapper>
+              <Icon><FaUser /></Icon>
               <Input type="text" placeholder="Apellido del participante" />
             </Field>
 
             <Field>
-              <IconWrapper><FaEnvelope /></IconWrapper>
+              <Icon><FaEnvelope /></Icon>
               <Input type="email" placeholder="Correo electrónico" />
             </Field>
 
             <Field>
-              <IconWrapper><FaPhone /></IconWrapper>
+              <Icon><FaPhone /></Icon>
               <Input type="tel" placeholder="Número de teléfono" />
             </Field>
 
             <Field>
-              <IconWrapper><FaListUl /></IconWrapper>
+              <Icon><FaListUl /></Icon>
               <Select>
                 <option>Elige una categoría</option>
                 <option>Naturaleza y aire libre</option>
@@ -52,7 +51,7 @@ const EventRegistrationForm = () => {
             </Field>
 
             <Field>
-              <IconWrapper><FaCalendarAlt /></IconWrapper>
+              <Icon><FaCalendarAlt /></Icon>
               <Input type="date" />
             </Field>
 
@@ -69,40 +68,35 @@ export default EventRegistrationForm;
 const Container = styled.div`
   display: flex;
   height: 100vh;
-  background-color: #18191a;
+  background-color: #ffffff;
   font-family: 'Arial', sans-serif;
 `;
 
 const ImageSection = styled.div`
   flex: 1;
   position: relative;
+
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
-    border-radius: 8px;
+    border-radius: 0px;
   }
 `;
 
-const Overlay = styled.div`
+const OverlayText = styled.div`
   position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #fff;
-  font-size: 24px;
-  font-weight: bold;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, 0.6);
+  color: white;
+  padding: 16px 32px;
+  border-radius: 8px;
+  font-size: 20px;
   text-align: center;
-  padding: 20px;
-`;
-
-const Phrase = styled.div`
-  z-index: 1;
+  line-height: 1.5;
+  font-weight: bold;
 `;
 
 const FormSection = styled.div`
@@ -110,17 +104,17 @@ const FormSection = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #FFF;
-  padding: 40px;
+  background-color: white;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+  border-radius: 16px;
+  padding: 15px;  
+  max-width: 320px; 
+  height: auto;  
+  margin: 20px;   
 `;
 
 const FormContainer = styled.div`
   width: 100%;
-  max-width: 400px;
-  background-color: #F7F9FB;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
 `;
 
 const Form = styled.form`
@@ -131,44 +125,46 @@ const Form = styled.form`
 const Field = styled.div`
   display: flex;
   align-items: center;
-  margin-bottom: 16px;
-  border: 1px solid #D0D7DF;
-  border-radius: 4px;
-  padding: 8px;
-  background-color: #FFF;
+  margin-bottom: 12px;
 `;
 
-const IconWrapper = styled.div`
+const Icon = styled.div`
+  color: #013B58;
+  font-size: 20px;
   margin-right: 8px;
-  color: #165252;
 `;
 
 const Input = styled.input`
-  width: 100%;
-  border: none;
-  outline: none;
-  padding: 8px;
-  font-size: 16px;
+  flex: 1;
+  padding: 10px;
+  border: 2px solid #165252;
+  border-radius: 8px;
+  font-size: 14px;
+  color: #3C4556;
+
+  &::placeholder {
+    color: #a0a0a0;
+  }
 `;
 
 const Select = styled.select`
-  width: 100%;
-  border: none;
-  outline: none;
-  padding: 8px;
-  font-size: 16px;
-  background-color: transparent;
+  flex: 1;
+  padding: 10px;
+  border: 2px solid #165252;
+  border-radius: 8px;
+  font-size: 14px;
+  color: #3C4556;
 `;
 
 const Button = styled.button`
-  width: 100%;
-  padding: 12px;
-  background-color: #165252;
+  padding: 10px 12px;
+  background-color: #78882D;
   color: white;
   border: none;
-  border-radius: 4px;
-  font-size: 16px;
+  border-radius: 8px;
+  font-size: 14px;
   cursor: pointer;
+  transition: background-color 0.3s ease;
 
   &:hover {
     background-color: #013B58;
